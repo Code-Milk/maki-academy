90437266156107cd34333e007eb9b01cf3f92f2e
Kelly Lim <37552758+Code-Milk@users.noreply.github.com>
2024-06-28 15:31:11 -0500

    Pomodoro Project v2

---
+++ JavaScript/Project/script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const completedCounter = document.getElementById("completed")
+
+function addTask() {
+    const task = inputBox.value.trim();
+    if (!task) {
+        alert("Please write down a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = 
+        `<label>
+            <input type="checkbox">
+            <span>${task}</span>
+        </label>
+        <span class="edit-btn">| Edit</span>
+        <span class="delete-btn">| Delete</span>`;
+
+    listContainer.appendChild(li);
+    inputBox.value = "";
+
+    const checkbox = li.querySelector("input");
+    const editBtn = li.querySelector(".edit-btn");
+    const taskSpan = li.querySelector("span");
+    const deleteBtn = li.querySelector(".delete-btn");
+
+    checkbox.addEventListener("click", function () {
+        listContainer.classList.toggle("completed", checkbox.checked);
+    });
+
+    editBtn.addEventListener("click", function () {
+        const update = prompt("Edit task:", taskSpan.textContent);
+        if (update !== null) {
+            taskSpan.textContent = update;
+            li.classList.remove("completed");
+        }
+        li.classList.remove("completed");
+    });
+
+
+}
