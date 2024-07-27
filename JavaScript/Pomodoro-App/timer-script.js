8724016f4a8b33ad1a4080385b72ab5da06cd9cd
Kelly Lim <kellylim2014@gmail.com>
2024-07-26 19:19:48 -0500

    Fixed Reset/Set Timer

--- JavaScript/Pomodoro-App/timer-script.js
+++ JavaScript/Pomodoro-App/timer-script.js
@@ -1,32 +1,27 @@
 const bells = new Audio('./sounds/bell.wav'); 
+// Feature Buttons
 const startBtn = document.querySelector('.btn-start');
 const resetBtn = document.querySelector('.btn-reset');
 const setTimerBtn = document.querySelector('.btn-set-time');
+// Pause Button
+const pauseBtn = document.createElement('button');
+pauseBtn.className = "btn-pause timer-btn";
+pauseBtn.textContent = "pause";
+// HTML Timer
 const sessionMin = document.querySelector('.minutes');
 const sessionSec = document.querySelector('.seconds');
+// Global variable for Set Time func
+let updateMin = 1;
 
+// Interval State
 let myInterval;
-let pausedInterval;
-let state = true; // true = off, false = on
+let timerOff = true; // true = off, false = on
 
-startBtn.addEventListener('click', function() {
+const pomodoroTimer = () => {
     const sessionAmount = (Number.parseInt(sessionMin.textContent) * 60) + (Number.parseInt(sessionSec.textContent));
 
-    // Replace Play Button with Pause
-    const pauseBtn = document.createElement('button');
-    pauseBtn.className = "btn-pause timer-btn";
-    pauseBtn.textContent = "pause";
-    startBtn.parentNode.replaceChild(pauseBtn, startBtn);
-
-    // Pause Timer
-    pauseBtn.addEventListener('click', () => {
-        clearInterval(myInterval);
-        state = true;
-        pauseBtn.parentNode.replaceChild(startBtn, pauseBtn);
-    });
-
-    if(state) {
-        state = false;
+    if(timerOff) {
+        timerOff = false;
         let totalSeconds = sessionAmount;
 
         const updateSeconds = () => {
@@ -55,27 +50,59 @@ startBtn.addEventListener('click', function() {
     } else {
         alert('Session has already started.')
     }
+};
+
+startBtn.addEventListener('click', function() {
+    // Replace Play Button with Pause
+    startBtn.parentNode.replaceChild(pauseBtn, startBtn);
+    pomodoroTimer();
+});
+
+// Pause Timer
+pauseBtn.addEventListener('click', () => {
+    clearInterval(myInterval);
+    timerOff = true;
+    pauseBtn.parentNode.replaceChild(startBtn, pauseBtn);
 });
 
 setTimerBtn.addEventListener('click', function() {
-    const setTimerMin = document.querySelector('.minutes');
-    const setTimerSec = document.querySelector('.seconds');
+    clearInterval(myInterval);
+    if(timerOff) { // Off
+        pauseBtn.parentNode.replaceChild(startBtn, pauseBtn); // Pause -> Start
+    } else { // On
+        timerOff = true;
+        pauseBtn.parentNode.replaceChild(startBtn, pauseBtn); // Pause -> Start
+    }
 
     // Prompt
-    let updateMin = parseInt(prompt("Edit Time:", setTimerMin.textContent));
-
-    // Error Checking
-    while (isNaN(updateMin)) {
-        updateMin = parseInt(prompt("This is not a number. Please enter a number for the timer.", ""))
-    }
+    updateMin = parseInt(prompt("Edit Time:", sessionMin.textContent));
     
-    // Set
-    setTimerMin.textContent = updateMin;
-    setTimerSec.textContent = '00';
+    // Error Checking
+    while (isNaN(updateMin) || (updateMin === 0)) { // Cannot start at 0
+        updateMin = parseInt(prompt("This is not valid. Please enter a number for the timer.", ""))
+    }
 
-    // Reset Time
-    resetBtn.addEventListener('click', function() {
-        setTimerMin.textContent = updateMin;
-        setTimerSec.textContent = '00';
-    });
+    // Set Timer
+    sessionMin.textContent = updateMin;
+    sessionSec.textContent = '00';
+});
+
+// Reset Time
+resetBtn.addEventListener('click', function() {
+    clearInterval(myInterval);
+    if(timerOff) { // Off
+        pauseBtn.parentNode.replaceChild(startBtn, pauseBtn); // Pause -> Start
+    } else { // On
+        timerOff = true;
+        pauseBtn.parentNode.replaceChild(startBtn, pauseBtn); // Pause -> Start
+    }
+
+    // Set timer
+    if(updateMin === 1) { // Default
+        sessionMin.textContent = 1;
+        sessionSec.textContent = '00';
+    } else { // Updated time
+        sessionMin.textContent = updateMin;
+        sessionSec.textContent = '00';
+    }
 });
